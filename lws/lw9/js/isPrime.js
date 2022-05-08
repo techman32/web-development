24e68f4954ab3f282cf02896540c36582192ec51
techman32 <klochko977@gmail.com>
2022-05-08 11:33:40 +0300

    new lw5-lw6

---
+++ lws/lw9/js/isPrime.js
@@ -0,0 +1,49 @@
+function isPrimeNumber(n) 
+{
+    if (typeof n === 'number') 
+    {
+        isSinglePrime(n); 
+    }
+    else if (Array.isArray(n)) 
+    {
+        for (let i = 0; i < n.length; i++) 
+        {
+            if (typeof n[i] === 'number') 
+            { 
+                isSinglePrime(n[i]);
+            }
+            else 
+            {
+                console.log(n[i] + ' is not a number');
+            }
+        }
+    }
+    else 
+    {
+        console.log(n + ' is not a number, neither an array');
+    }
+}
+  
+    function isSinglePrime(x) 
+    {
+        let isPrime = true;
+  
+        if (!Number.isInteger(x) || x <= 1) 
+        {
+            isPrime = false;
+        }
+  
+        for (let i = 2; i < x && isPrime; i++) 
+        {
+            if (x % i === 0) 
+            {
+                isPrime = false;
+                break;
+            }
+        }
+  
+        if (isPrime)
+            console.log(x + ' is a prime number');
+        else
+            console.log(x + ' is not a prime number');
+    }
